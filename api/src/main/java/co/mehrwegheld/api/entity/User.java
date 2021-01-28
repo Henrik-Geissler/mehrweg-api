9906d2d2cbd2d019b14d90947d10908693bc4a26
Manuel Müller <urmse@student.kit.edu>
2021-01-28 10:04:02 +0100

    add working example REST endpoint with error handling

--- api/src/main/java/co/mehrwegheld/api/entity/User.java
+++
@@ -1,4 +0,0 @@
-package co.mehrwegheld.api.entity;
-
-public class User {
-}
---
+++ api/src/main/java/co/mehrwegheld/api/model/db/User.java
@@ -0,0 +1,28 @@
+package co.mehrwegheld.api.model.db;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String name;
+    private String username;
+    private String password;
+    private String email;
+
+    public User(String name, String username, String password, String email) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+}
